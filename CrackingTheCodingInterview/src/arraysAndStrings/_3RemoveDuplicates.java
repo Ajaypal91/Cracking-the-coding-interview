f42848b80422cb812ec5db2503110a5dd923673d
Ajaypal91 <erajaypal91@gmail.com>
2018-01-08 16:46:38 -0800

    adding new solutions

---
+++ CrackingTheCodingInterview/src/arraysAndStrings/_3RemoveDuplicates.java
@@ -0,0 +1,34 @@
+package arraysAndStrings;
+
+import java.util.Scanner;
+
+public class _3RemoveDuplicates {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.nextLine();
+		
+		System.out.println(removeDuplicates(s));
+		
+		sc.close();
+
+	}
+	
+	private static String removeDuplicates(String s){
+		
+		StringBuilder sb =  new StringBuilder();
+		int checker = 0;
+		for (char c : s.toCharArray()){
+			int val = (int)(c-'a');
+			if((checker & (1 << val)) == 0)
+				sb.append(c);
+			checker |= 1 << val;
+			
+		}
+		
+		return sb.toString();
+	}
+
+}
